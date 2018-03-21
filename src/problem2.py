4690a91427c517ce2db62c139d80ba50f1a61c98
max3 <max3@rose-hulman.edu>
2018-03-20 21:39:30 -0400

    DONE

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 Exam 1, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  March 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Xiaoyu Ma.  March 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -114,8 +114,18 @@ def problem2(line1, line2, thickness, win):
       :type thickness:  int
       :type win:        rg.RoseWindow
     """
+    line1.attach_to(win)
+    line2.attach_to(win)
+
+    corner1 = line1.get_midpoint()
+    corner2 = line2.get_midpoint()
+    rectangle = rg.Rectangle(corner1,corner2)
+    rectangle.outline_thickness = thickness
+    rectangle.outline_color = line1.color
+    rectangle.attach_to(win)
+    win.render()
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function, TESTING each step as you go.
+    # DONE: 2. Implement and test this function, TESTING each step as you go.
     #          Tests have been written for you (above).
     #   See the IMPORTANT NOTE just above the DEF line above.
     # -------------------------------------------------------------------------
